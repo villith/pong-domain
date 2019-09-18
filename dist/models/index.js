0adf8fa42043edd80fc09b8767d707a1d0412f39
Scott Donnelly <scoot.donnelly@gmail.com>
2019-09-17 21:01:12 -0400

    Commit generated files

--- dist/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Player_1 = require("./Player");
-exports.PlayerModel = Player_1.default;
-var Match_1 = require("./Match");
-exports.MatchModel = Match_1.default;
-var Result_1 = require("./Result");
-exports.ResultModel = Result_1.default;
