60e3c0c83c47139200e2e24abd328667712838e2
Scott Donnelly <scoot.donnelly@gmail.com>
2019-09-22 20:04:16 -0400

    add prop decorators

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -4,10 +4,19 @@ class Player extends Typegoose {
   @prop({ unique: true, required: true })
   name: string;
   
+  @prop()
   nickname?: string;
+
+  @prop()
   emojiFlair?: string;
+
+  @prop()
   tagline?: string;
+
+  @prop()
   victoryLine?: string;
+
+  @prop()
   defeatLine?: string;
 
   @prop({ default: 1500 })
